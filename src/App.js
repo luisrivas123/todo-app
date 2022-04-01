c2764a8fa5fb67679cca611b98884f35c36d8d2f
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-04-01 14:08:01 -0500

    Manejo de eventos y estado, filtrar y buscar TODOs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+// import './App.css';
+
+const defaultTodos = [
+  {text: 'Diseñar', completed: true},
+  {text: 'Programar', completed: false},
+  {text: 'Testing', completed: false},
+  {text: 'Producción', completed: false},
+];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todos => !!todos.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue >= 1){
+    searchedTodos = todos;
+  }else{
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<div>
+    <React.Fragment>
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      {/* <h2>Has completado 2 de 3 TODOs</h2> */}
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+      {/* <input placeholder="TODO-app" /> */}
+      <TodoList>
+      {searchedTodos.map(todo => (
+        <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}
+        />
+      ))}
+      </TodoList>
+      <CreateTodoButton />
+      {/* <button>+</button> */}
+    </React.Fragment>
+    //</div>
   );
 }
 
---
+++ src/TodoSearch.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './TodoSearch.css';
+
+function TodoSearch({searchValue, setSearchValue}) {
+
+
+    
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+        setSearchValue(event.target.value);
+    }
+    
+    return(
+        <input 
+            className="TodoSearch"
+            placeholder="TODO-app"
+            value={ searchValue }
+            onChange={ onSearchValueChange }
+        />
+    );
+
+    // return[
+    //     <input 
+    //         className="TodoSearch" 
+    //         placeholder="TODO-app"
+    //         value={ searchValue }
+    //         onChange={ onSearchValueChange }
+    //     />,
+    //     <p>{ searchValue }</p>
+    // ];
+}
+
+export { TodoSearch };
