0456e31013a2522e05efd7c6c72aeefd38df234a
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-04-04 23:51:00 -0500

    Modal, formulario añadir TODOs y TodoForm.css

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -3,14 +3,14 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
     
-    const onClickButton = (msg) => {
-        alert(msg);
+    const onClickButton = () => {
+        props.setOpenModal(prevState => !prevState);
     }
 
     return(
         <button 
             className="CreateTodoButton"
-            onClick={ () => onClickButton() }
+            onClick={ onClickButton }
         >
             +
         </button>
