1bd819b45989eb5a9b9c8164e8bff3043361adfd
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-04-07 21:36:19 -0500

    Composición uso del contexto

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,57 @@ import React from "react";
 import { TodoProvider } from "../TodoContext";
 import { AppUI } from "./AppUI";
 
-function App() {
+function App(){
+  const [state, setState] = React.useState('');
 
-  return (
+  return(
     <TodoProvider>
-      <AppUI />
+      <React.Fragment>
+        <TodoHeader />
+        <TodoList />
+      </React.Fragment>
     </TodoProvider>
   );
 }
+
+function TodoHeader(){
+  return(
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+    </React.Fragment>
+  );
+}
+
+function TodoList(){
+  return(
+    <React.Fragment>
+      <TodoItem />
+    </React.Fragment>
+  );
+}
+
+function TodoCounter(){
+  const [state] = React.useContext(TodoProvider);
+
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch(){
+  return <p>TodoSearchr</p>;
+}
+
+function TodoItem(){
+  return <p>TodoItem</p>;
+}
+
+// function App() {
+
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -3,10 +3,11 @@ import './TodosLoading.css';
 
 function TodosLoading () {
     return (
-        <div>
+        <div className="LoadingTodo-container">
             <span className="LoadinTodo-container"></span>
             <p className="LoadingTodo-text">Cargando TODOs</p>
             <span className="LoadingTodo-deleteIcon"></span>
+            <span className="LoadindTodo-deleteIcon"></span>
         </div>
     );
 }
